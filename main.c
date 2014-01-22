f19583ef325da229216fc38101ab48862c26f375
m1cky <mabir4@students.d125.org>
2014-01-21 23:34:31 -0600

    Struct Pri UnCon

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 
 int main()
 {
-  printf("Cross-computer testing!\n");
+  Pri vector;
+  vector.x = 2;
+  vector.y = 3;
+  vector.z = 4;
+
+  printf("Cross-computer testing! X: %f Y: %f Z: %f\n", vector.x, vector.y, vector.z);
   return 0;
 }
--- pri.h
+++ pri.h
@@ -1,2 +1,13 @@
 #include "stdio.h"
 #include "math.h"
+
+/* Comparable to a Vector */
+typedef struct
+{
+  double x, y, z;
+} Pri;
+
+typedef struct
+{
+
+} Flavor;
