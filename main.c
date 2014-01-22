4bc834176e2ae1537769cc175fa62f1967cc132a
m1cky <mabir4@students.d125.org>
2014-01-22 14:22:35 -0600

    No Changes

--- main.c
+++ main.c
@@ -1,12 +1,9 @@
 #include "pri.h"
 
+
+
 int main()
 {
-  Pri vector;
-  vector.x = 2;
-  vector.y = 3;
-  vector.z = 4;
-
   printf("Cross-computer testing! X: %f Y: %f Z: %f\n", vector.x, vector.y, vector.z);
   return 0;
 }
